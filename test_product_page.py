36d4281c3a36ef5902019794b166fff09b8d1ef5
ShelpyakovAS <Shelpchel@gmail.com>
2022-11-11 12:22:28 +0500

    pep8

--- test_product_page.py
+++ test_product_page.py
@@ -9,6 +9,7 @@ def test_guest_can_add_product_to_basket(browser):
     page.add_product_in_basket()
     page.check_product_in_basket()
 
+
 @pytest.mark.parametrize('link_i', [i for i in range(10)])
 def test_guest_can_add_product_to_basket_promo(browser, link_i):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer' + str(link_i)
